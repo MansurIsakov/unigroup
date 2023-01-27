c2632ee4305456f906445bfd498b211341c64419
MansurIsakov <mansurisakov.dev@gmail.com>
2023-01-27 20:44:26 +0500

    feat: minor changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoadingSpinnerComponent } from './shared/components/loading-spinner/loading-spinner.component';
 import { MainComponent } from './features/main/main.component';
+import { HeaderComponent } from './core/components/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, LoadingSpinnerComponent, MainComponent],
+  declarations: [
+    AppComponent,
+    LoadingSpinnerComponent,
+    MainComponent,
+    HeaderComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
